8ff726271cb6e88ce5aa450cd2afc82da8acbbc1
Nicci Sheets <niccisheets@aol.com>
2018-06-11 11:30:33 -0400

    added math operator defs

---
+++ calculator_app.rb
@@ -0,0 +1,23 @@
+def addition(num1,num2)
+	num1 + num2
+end
+
+p addition(4,2)
+
+def subtraction(num1,num2)
+	num1 - num2
+end
+
+p subtraction(4,2)
+
+def multiplication(num1,num2)
+	num1 * num2
+end
+ 
+p multiplication(4,2)
+
+def division(num1,num2)
+	num1/num2
+end
+
+p division(3,2)
